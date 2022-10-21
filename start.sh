b9dd50c8ce48a8309b55a8422794f12d7b0cc5c8
Nicolas Giard <github@ngpixel.com>
2022-10-21 19:11:34 -0400

    chore: update start.sh

--- start.sh
+++ start.sh
@@ -3,15 +3,13 @@
 ORGANIZATION=$ORGANIZATION
 ACCESS_TOKEN=$ACCESS_TOKEN
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
+./config.sh --url https://github.com/${ORGANIZATION} --token ${ACCESS_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${ACCESS_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
